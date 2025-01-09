6b13b8fbf5607725bb9a1c69d7e87ac512a24c80
Hamster-crab <hhidekitano@gmail.com>
2025-01-09 21:49:57 +0900

    test

--- src/main.cpp
+++ src/main.cpp
@@ -1,239 +1,188 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <math.h>
-#include <random>
-#include <vector>
-#include <GL/glut.h>
+#include <GLFW/glfw3.h>
+#include "../include/glad/glad.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
 
-// ウィンドウ設定
-#define WINDOW_X (500)
-#define WINDOW_Y (500)
-#define WINDOW_NAME "test3"
+// タイルのサイズ（1タイルの大きさ）
+const float TILE_SIZE = 1.0f;
 
-void init_GL(int argc, char *argv[]);
-void init();
-void set_callback_functions();
+// 頂点データ（1つのタイルの四角形）
+float vertices[] = {
+    // 座標           // 色 (白色)
+    -0.5f, -0.5f, 0.0f,  1.0f, 1.0f, 1.0f, // 左下
+     0.5f, -0.5f, 0.0f,  1.0f, 1.0f, 1.0f, // 右下
+     0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 1.0f, // 右上
+    -0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 1.0f  // 左上
+};
 
-void glut_display();
-void glut_keyboard(unsigned char key, int x, int y);
-void glut_mouse(int button, int state, int x, int y);
-void glut_motion(int x, int y);
-void glut_idle();
+// インデックスデータ（四角形の2つの三角形）
+unsigned int indices[] = {
+    0, 1, 2,
+    2, 3, 0
+};
 
-void draw_raindrop();
-void draw_waves();
+// シェーダーソースコード（シンプルな白色で描画）
+const char* vertexShaderSource = R"(
+    #version 330 core
+    layout(location = 0) in vec3 aPos;
+    layout(location = 1) in vec3 aColor;
+    out vec3 fragColor;
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 projection;
+    void main() {
+        fragColor = aColor;
+        gl_Position = projection * view * model * vec4(aPos, 1.0);
+    }
+)";
 
-typedef struct{
-    double x;
-    double z;
-    double r;
-    double t;
-} wave;
+const char* fragmentShaderSource = R"(
+    #version 330 core
+    in vec3 fragColor;
+    out vec4 FragColor;
+    void main() {
+        FragColor = vec4(fragColor, 1.0);
+    }
+)";
 
-std::vector<std::vector<double>> rains;
-std::vector<wave> waves;
-std::random_device rnd;
+// コンパイルとリンクを行う関数
+unsigned int compileShader(unsigned int type, const char* source) {
+    unsigned int shader = glCreateShader(type);
+    glShaderSource(shader, 1, &source, NULL);
+    glCompileShader(shader);
 
-bool start_flag = false;
-int rain_rate = 3;
-
-// グローバル変数
-double g_angle1 = 0.0;
-double g_angle2 = 0.0;
-double g_distance = 20.0;
-bool g_isLeftButtonOn = false;
-bool g_isRightButtonOn = false;
-
-int main(int argc, char *argv[]){
-    init_GL(argc,argv);
-    init();
-    set_callback_functions();
-    glutMainLoop();
-
-    return 0;
-}
-void init_GL(int argc, char *argv[]){
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
-    glutInitWindowSize(WINDOW_X,WINDOW_Y);
-    glutCreateWindow(WINDOW_NAME);
-}
-
-void init(){
-    glClearColor(0.0, 0.0, 0.0, 0.0); // 背景の塗りつぶし色を指定
-}
-
-void set_callback_functions(){
-    glutDisplayFunc(glut_display);
-    glutKeyboardFunc(glut_keyboard);
-    glutMouseFunc(glut_mouse);
-    glutMotionFunc(glut_motion);
-    glutPassiveMotionFunc(glut_motion);
-    glutIdleFunc(glut_idle);
+    int success;
+    char infoLog[512];
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, 512, NULL, infoLog);
+        std::cout << "Shader Compilation Failed\n" << infoLog << std::endl;
+    }
+    return shader;
 }
 
-void glut_keyboard(unsigned char key, int x, int y){
-    switch(key){
-    // 終了操作
-    case 'q':
-    case 'Q':
-    case '\033':
-        exit(0);
-        break;
+unsigned int createShaderProgram(const char* vertexSource, const char* fragmentSource) {
+    unsigned int vertexShader = compileShader(GL_VERTEX_SHADER, vertexSource);
+    unsigned int fragmentShader = compileShader(GL_FRAGMENT_SHADER, fragmentSource);
 
-    case 's':  // start
-        start_flag = start_flag ? false : true;
-        break;
-    // 雨量変更
-    case '0': rain_rate = 0; break;
-    case '1': rain_rate = 1; break;
-    case '2': rain_rate = 2; break;
-    case '3': rain_rate = 3; break;
-    case '4': rain_rate = 4; break;
-    case '5': rain_rate = 5; break;
-    case '6': rain_rate = 6; break;
-    case '7': rain_rate = 7; break;
-    case '8': rain_rate = 8; break;
-    case '9': rain_rate = 9; break;
-    case '-': rain_rate = 10; break;
+    unsigned int shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    int success;
+    char infoLog[512];
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "Program Linking Failed\n" << infoLog << std::endl;
     }
 
-    glutPostRedisplay();
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    return shaderProgram;
 }
 
-// マウス操作
-void glut_mouse(int button, int state, int x, int y){
-    if(button == GLUT_LEFT_BUTTON){
-        if(state == GLUT_UP){
-            g_isLeftButtonOn = false;
-        }else if(state == GLUT_DOWN){
-            g_isLeftButtonOn = true;
-        }
-    }
-
-    if(button == GLUT_RIGHT_BUTTON){
-        if(state == GLUT_UP){
-            g_isRightButtonOn = false;
-        }else if(state == GLUT_DOWN){
-            g_isRightButtonOn = true;
-        }
-    }
-}
-
-// マウス移動
-void glut_motion(int x, int y){
-    static int px = -1, py = -1;
-    if(g_isLeftButtonOn == true){
-        if(px >= 0 && py >= 0){
-            g_angle1 += (double)-(x - px)/20;
-            g_angle2 += (double)(y - py)/20;
-        }
-        px = x;
-        py = y;
-    }else if(g_isRightButtonOn == true){
-        if(px >= 0 && py >= 0){
-            g_distance += (double)(y - py)/20;
-        }
-        px = x;
-        py = y;
-    }else{
-        px = -1;
-        py = -1;
-    }
-    glutPostRedisplay();
-}
-
-// 描画
-void glut_display(){
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(30.0, 1.0, 0.1, 100);
-
-    glMatrixMode(GL_MODELVIEW);
-    glLoadIdentity();
-    if (cos(g_angle2)>0){
-        gluLookAt(g_distance * cos(g_angle2) * sin(g_angle1),
-                    g_distance * sin(g_angle2),
-                    g_distance * cos(g_angle2) * cos(g_angle1),
-                    0.0, 0.0, 0.0, 0.0, 1.0, 0.0);}
-    else{
-        gluLookAt(g_distance * cos(g_angle2) * sin(g_angle1),
-                    g_distance * sin(g_angle2),
-                    g_distance * cos(g_angle2) * cos(g_angle1),
-                    0.0, 0.0, 0.0, 0.0, -1.0, 0.0);}
-
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+void setupOpenGL() {
+    // OpenGL設定
     glEnable(GL_DEPTH_TEST);
-
-    draw_raindrop();
-    draw_waves();
-
-    glFlush();
-    glDisable(GL_DEPTH_TEST);
-
-    glutSwapBuffers();
+    glClearColor(0.5f, 0.5f, 0.5f, 1.0f);  // 背景色（灰色）
 }
 
-// ループ処理
-void glut_idle(){
-    int count_rain = 0, count_wave = 0;
-    for(int i=0; i<rains.size(); i++){
-        rains[i][1] -= 0.1;
-        if((double)rains[i][1]-0.5 < -4.0) count_rain++;
+int main() {
+    // GLFW初期化
+    if (!glfwInit()) {
+        std::cerr << "GLFW Initialization Failed!" << std::endl;
+        return -1;
     }
-    for(int i=0; i<waves.size(); i++){
-        waves[i].r += 0.05; waves[i].t -= 0.03;
-        if(waves[i].t < 0) count_wave++;
-    }
-    if(count_rain > 0){
-        for(int i=0; i<count_rain; i++){
-            wave new_w;
-            new_w.x = rains[i][0]; new_w.z = rains[i][2];
-            new_w.r = 0.01; new_w.t = 1.0;
-            waves.push_back(new_w);
-        }
-        rains.erase(rains.begin(), rains.begin()+count_rain);
-    }
-    if(count_wave > 0) waves.erase(waves.begin(), waves.begin()+count_wave);
-    if(start_flag){
-        if(rnd()%10 < rain_rate){
-            std::vector<double> new_v = {(double)(rnd()%400)/50.0-4.0, 4.0, (double)(rnd()%400)/50.0-4.0};
-            rains.push_back(new_v);
-        }
-    }
-    usleep(10000); // フレームレート調整
-    glutPostRedisplay();
-}
 
-void draw_raindrop(){
-    glColor3d(1.0, 1.0, 1.0);
-    glLineWidth(1.0);
-    for(int i=0; i<rains.size(); i++){
-        glBegin(GL_LINES);
-        glVertex3d(rains[i][0], rains[i][1]-0.5, rains[i][2]);
-        glVertex3d(rains[i][0], rains[i][1]+0.5, rains[i][2]);
-        glEnd();
+    // ウィンドウ作成
+    GLFWwindow* window = glfwCreateWindow(800, 600, "White Tile Floor", nullptr, nullptr);
+    if (!window) {
+        std::cerr << "GLFW Window Creation Failed!" << std::endl;
+        glfwTerminate();
+        return -1;
     }
-}
+    glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+        glViewport(0, 0, width, height);
+    });
 
-void draw_waves(){
-    double x, z, r, color;
-    for(int i=0; i<waves.size(); i++){
-        x = waves[i].x; z = waves[i].z;
-        r = waves[i].r; color = waves[i].t;
+    // glad初期化
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cerr << "GLAD Initialization Failed!" << std::endl;
+        return -1;
+    }
 
-        glColor3d(color, color, color);
-        glBegin(GL_LINE_LOOP);
-        for(double j=0.0; j<50; j++){
-            glVertex3d(r*cos(2.0*M_PI*j/50.0)+x, -4.0, r*sin(2.0*M_PI*j/50.0)+z);
+    // シェーダープログラム作成
+    unsigned int shaderProgram = createShaderProgram(vertexShaderSource, fragmentShaderSource);
+
+    // VAO、VBO、EBO作成
+    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    // 頂点属性の設定
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // カメラビュー行列と投影行列
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -5.0f));
+
+    // メインループ
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // タイルを並べるための行列
+        glUseProgram(shaderProgram);
+
+        unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
+        unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
+        unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+        glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
+
+        // タイルを並べる
+        int gridSize = 10; // 10x10のタイルを描画
+        for (int i = 0; i < gridSize; ++i) {
+            for (int j = 0; j < gridSize; ++j) {
+                glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(i * TILE_SIZE, 0.0f, j * TILE_SIZE));
+                glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+                
+                glBindVertexArray(VAO);
+                glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+            }
         }
-        glEnd();
-        glBegin(GL_LINE_LOOP);
-        for(double j=0.0; j<50; j++){
-            glVertex3d((r+0.03)*cos(2.0*M_PI*j/50.0)+x, -4.0, (r+0.03)*sin(2.0*M_PI*j/50.0)+z);
-        }
-        glEnd();
-    }       
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // リソースの解放
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+
+    glfwTerminate();
+    return 0;
 }
 
